7fec4148ce79c4baab95d603459a89ae3b1de766
sohag-molla <pcgamers579@gmasil.com>
2024-05-30 01:04:16 +0600

    First Comment

---
+++ date.js
@@ -0,0 +1,20 @@
+// formating//
+
+// let myDate = new Date("2024-05-29");
+// console.log(myDate)
+
+//get date mathod//
+
+//  let myDate = new Date();
+
+//  let currentYear = myDate.getDay();
+//  console.log(currentYear)
+
+ //set method//
+
+let d = new Date();
+
+// d.setFullYear(2022,7,15);
+// d.setMonth(11)
+d.setMinutes(59)
+console.log(d)
---
+++ math.js
@@ -0,0 +1,65 @@
+//Math round// 
+ 
+//  let math = Math.round(9.5);
+//  console.log(math)
+
+
+//Math Ceil//
+let ceil = Math.ceil(11.1);
+//console.log(ceil)
+
+//Math Floor//
+
+let floor = Math.floor(10.9);
+//console.log(floor)
+
+//math trunc//{Note:floor and trunc almost fully same}
+
+let trunc = Math.trunc(1.9);
+//console.log(trunc)
+
+
+
+//Math sign//{-100=-1,100=1,sohag=NaN}
+
+let sign = Math.sign("sohag");
+//console.log(sign)
+
+
+
+//Math paw..er//{squre ar kaj kora (,2:8*8=64)}
+
+let power = Math.pow(10,5);
+//console.log(power)
+
+//Math Square// {borgoMul ar kaj kora.jake bola root}
+
+let square = Math.sqrt(100000);
+// console.log(square)
+
+
+//Math Absolute// {Negative ka positive kora}
+
+let absolute = Math.abs(-400.5);
+// console.log(absolute)
+
+
+// Math Main// {sob tka soto sonkha ka ber kora}
+
+
+let main = Math.min(1,2,3,4,5,-10,-100);
+
+//console.log(main)
+
+
+//Math Max// {sorbocco sonkha ka ber kora}
+
+let max = Math.max(-1,-2,-3,0,1,2,3,4,5,6,10,20);
+
+//console,log(max)
+
+
+//Math Random// {ar maxium valu 1 ar necha takba all time}
+
+let random = Math.random();
+console.log(random)
